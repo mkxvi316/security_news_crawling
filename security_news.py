6ef13d3f5e4bd7a3bb89c6fe753d49a24d17b215
mkxvi316 <jihyeok316@naver.com>
2025-03-19 12:28:38 +0900

    3/19(wed) security news updated

--- security_news.py
+++ security_news.py
@@ -92,7 +92,7 @@ class BoanCrawler:
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>보안 뉴스</title>
+            <title>&#128680;보안 뉴스</title>
             <style>
                 body {{ font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; }}
                 h1 {{ color: #333; text-align: center; }}
